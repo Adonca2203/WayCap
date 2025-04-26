551732e4ced03bc2a1fba49224e425285cb41080
Adonis Carvajal <adonis.carvajal2203@gmail.com>
2025-04-25 22:18:40 -0400

    Added audio encoder tests

--- src/encoders/audio_encoder.rs
+++ src/encoders/audio_encoder.rs
@@ -9,20 +9,134 @@ use super::{buffer::AudioBuffer, video_encoder::ONE_MICROS};
 
 const MIN_RMS: f32 = 0.01;
 
-pub struct AudioEncoder {
-    encoder: Option<ffmpeg::codec::encoder::Audio>,
+pub struct FfmpegAudioEncoder(ffmpeg::codec::encoder::Audio);
+
+pub trait AudioEncoderImpl {
+    type Error;
+
+    fn codec(&self) -> Option<ffmpeg::Codec>;
+    fn time_base(&self) -> Rational;
+
+    fn channels(&self) -> u16;
+    fn frame_size(&self) -> i32;
+    fn format(&self) -> ffmpeg::format::Sample;
+    fn channel_layout(&self) -> ffmpeg::channel_layout::ChannelLayout;
+    fn rate(&self) -> u32;
+
+    fn send_frame(&mut self, frame: &ffmpeg::frame::Audio) -> Result<(), Self::Error>;
+    fn receive_packet(
+        &mut self,
+        pkt: &mut ffmpeg::codec::packet::Packet,
+    ) -> Result<(), Self::Error>;
+    fn send_eof(&mut self) -> Result<(), Self::Error>;
+}
+
+impl AudioEncoderImpl for FfmpegAudioEncoder {
+    type Error = ffmpeg::Error;
+
+    fn codec(&self) -> Option<ffmpeg_next::Codec> {
+        self.0.codec()
+    }
+
+    fn time_base(&self) -> Rational {
+        self.0.time_base()
+    }
+
+    fn channels(&self) -> u16 {
+        self.0.channels()
+    }
+
+    fn frame_size(&self) -> i32 {
+        unsafe { (*self.0.as_ptr()).frame_size }
+    }
+
+    fn format(&self) -> ffmpeg_next::format::Sample {
+        self.0.format()
+    }
+
+    fn channel_layout(&self) -> ffmpeg_next::channel_layout::ChannelLayout {
+        self.0.channel_layout()
+    }
+
+    fn rate(&self) -> u32 {
+        self.0.rate()
+    }
+
+    fn send_frame(&mut self, frame: &ffmpeg_next::frame::Audio) -> Result<(), Self::Error> {
+        self.0.send_frame(frame)
+    }
+
+    fn receive_packet(
+        &mut self,
+        pkt: &mut ffmpeg_next::codec::packet::Packet,
+    ) -> Result<(), Self::Error> {
+        self.0.receive_packet(pkt)
+    }
+
+    fn send_eof(&mut self) -> Result<(), Self::Error> {
+        self.0.send_eof()
+    }
+}
+
+impl FfmpegAudioEncoder {
+    pub fn new_opus() -> Result<Self, ffmpeg::Error> {
+        let encoder_codec = ffmpeg::codec::encoder::find(ffmpeg_next::codec::Id::OPUS)
+            .ok_or(ffmpeg::Error::EncoderNotFound)?;
+
+        let mut encoder_ctx = ffmpeg::codec::context::Context::new_with_codec(encoder_codec)
+            .encoder()
+            .audio()?;
+
+        encoder_ctx.set_rate(48000);
+        encoder_ctx.set_bit_rate(70_000);
+        encoder_ctx.set_format(ffmpeg::format::Sample::F32(
+            ffmpeg_next::format::sample::Type::Packed,
+        ));
+        encoder_ctx.set_time_base(Rational::new(1, 48000));
+        encoder_ctx.set_frame_rate(Some(Rational::new(1, 48000)));
+        encoder_ctx.set_channel_layout(ffmpeg::channel_layout::ChannelLayout::STEREO);
+
+        let mut encoder = encoder_ctx.open()?;
+
+        // Opus frame size is based on n channels so need to update it
+        unsafe {
+            (*encoder.as_mut_ptr()).frame_size =
+                (encoder.frame_size() as i32 * encoder.channels() as i32) as i32;
+        }
+
+        Ok(FfmpegAudioEncoder(encoder))
+    }
+}
+
+impl AsRef<ffmpeg::codec::encoder::Audio> for FfmpegAudioEncoder {
+    fn as_ref(&self) -> &ffmpeg::codec::encoder::Audio {
+        &self.0
+    }
+}
+
+pub struct AudioEncoder<E>
+where
+    E: AudioEncoderImpl,
+{
+    encoder: Option<E>,
     audio_buffer: AudioBuffer,
     next_pts: i64,
     leftover_data: VecDeque<f32>,
 }
 
-impl AudioEncoder {
-    pub fn new(max_seconds: u32) -> Result<Self, ffmpeg::Error> {
-        let encoder = Some(Self::create_opus_encoder()?);
+impl<E> AudioEncoder<E>
+where
+    E: AudioEncoderImpl<Error = ffmpeg::Error>,
+{
+    pub fn new_with_factory(
+        factory: impl Fn() -> Result<E, ffmpeg::Error>,
+        max_seconds: u32,
+    ) -> Result<Self, ffmpeg::Error> {
+        let encoder = factory()?;
         let max_time = max_seconds as usize * ONE_MICROS;
 
         Ok(Self {
-            encoder,
+            encoder: Some(encoder),
             audio_buffer: AudioBuffer::new(max_time),
             next_pts: 0,
             leftover_data: VecDeque::new(),
@@ -78,7 +192,7 @@ impl AudioEncoder {
         Ok(())
     }
 
-    pub fn get_encoder(&self) -> &Option<ffmpeg::codec::encoder::Audio> {
+    pub fn get_encoder(&self) -> &Option<E> {
         &self.encoder
     }
 
@@ -106,43 +220,21 @@ impl AudioEncoder {
         self.audio_buffer.reset();
     }
 
-    pub fn reset_encoder(&mut self) -> Result<(), ffmpeg::Error> {
+    pub fn reset_encoder(
+        &mut self,
+        factory: impl Fn() -> Result<E, ffmpeg::Error>,
+    ) -> Result<(), ffmpeg::Error> {
         self.drop_encoder();
+        self.encoder = Some(factory()?);
 
-        self.encoder = Some(Self::create_opus_encoder()?);
         Ok(())
     }
 }
 
-impl AudioEncoder {
-    fn create_opus_encoder() -> Result<ffmpeg::codec::encoder::Audio, ffmpeg::Error> {
-        let encoder_codec = ffmpeg::codec::encoder::find(ffmpeg_next::codec::Id::OPUS)
-            .ok_or(ffmpeg::Error::EncoderNotFound)?;
-
-        let mut encoder_ctx = ffmpeg::codec::context::Context::new_with_codec(encoder_codec)
-            .encoder()
-            .audio()?;
-
-        encoder_ctx.set_rate(48000);
-        encoder_ctx.set_bit_rate(70_000);
-        encoder_ctx.set_format(ffmpeg::format::Sample::F32(
-            ffmpeg_next::format::sample::Type::Packed,
-        ));
-        encoder_ctx.set_time_base(Rational::new(1, 48000));
-        encoder_ctx.set_frame_rate(Some(Rational::new(1, 48000)));
-        encoder_ctx.set_channel_layout(ffmpeg::channel_layout::ChannelLayout::STEREO);
-
-        let mut encoder = encoder_ctx.open()?;
-
-        // Opus frame size is based on n channels so need to update it
-        unsafe {
-            (*encoder.as_mut_ptr()).frame_size =
-                (encoder.frame_size() as i32 * encoder.channels() as i32) as i32;
-        }
-
-        Ok(encoder)
-    }
-
+impl<E> AudioEncoder<E>
+where
+    E: AudioEncoderImpl<Error = ffmpeg::Error>,
+{
     fn boost_with_rms(samples: &mut [f32]) -> Result<(), ffmpeg::Error> {
         let sum_sqrs = samples.iter().map(|&s| s * s).sum::<f32>();
         let rms = (sum_sqrs / samples.len() as f32).sqrt();
--- src/encoders/mod.rs
+++ src/encoders/mod.rs
@@ -2,6 +2,8 @@ pub mod audio_encoder;
 pub mod buffer;
 #[cfg(test)]
 mod buffer_tests;
+#[cfg(test)]
+mod audio_encoder_tests;
 pub mod nvenc_encoder;
 pub mod vaapi_encoder;
 pub mod video_encoder;
